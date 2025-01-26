a54c41d0e291ace3b5fae952cd42c9c082282645
Maxwellington <maxwellington@maxwellgtonsmbp.lan>
2025-01-26 13:24:54 -0600

    Add React import to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import app from './firebase';
+
+// Import pages
+import Home from './pages/Home';
+import GetPluggedIn from './pages/GetPluggedIn';
+import Resources from './pages/Resources';
+import Calendar from './pages/Calendar';
+import Partnerships from './pages/Partnerships';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/get-plugged-in">Get Plugged In</Link>
+          <Link to="/resources">Resources</Link>
+          <Link to="/calendar">Calendar</Link>
+          <Link to="/partnerships">Partnerships</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/get-plugged-in" element={<GetPluggedIn />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/partnerships" element={<Partnerships />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
